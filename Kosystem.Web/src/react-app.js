ff18afa0115b73e855b74967b41e91382bdffee3
kalle (jag) <kalle.jillheden@gmail.com>
2019-08-23 17:42:39 +0200

    Reverted to empty project

--- Kosystem.Web/src/react-app.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './react-app/App';
-ReactDOM.render(React.createElement(App, null), document.getElementById('react-app'));
-//# sourceMappingURL=react-app.js.map
--- Kosystem.Web/src/react-app.tsx
+++
@@ -1,9 +0,0 @@
-﻿import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import App from './react-app/App';
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById('react-app')
-);
