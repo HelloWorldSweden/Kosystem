d59bae9496aa07e79fa3d43ccf58679743dd4162
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-21 22:14:56 +0100

    Revert "Added basic classes for room/person middlewares"
    This reverts commit 961470983de1507ebaec2e03b88787f10f60e0dc.

--- Kosystem/Startup.cs
+++ Kosystem/Startup.cs
@@ -47,11 +47,7 @@ namespace Kosystem
                 {
                     sqliteOpt.MigrationsAssembly(nameof(Kosystem));
                 });
-            })
-                .AddPersonRepositoryMiddleware((p, repo)
-                    => new EventAwarePersonRepository(repo, p.GetRequiredService<IKosystemEvents>()))
-                .AddRoomRepositoryMiddleware((p, repo)
-                    => new EventAwareRoomRepository(repo, p.GetRequiredService<IKosystemEvents>()));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
