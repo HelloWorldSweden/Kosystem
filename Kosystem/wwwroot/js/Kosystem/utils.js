3112ca48b2e59ca4ea56f1fa48ff3be7c85b9ec8
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-30 00:07:55 +0100

    Small js refactorings

--- Kosystem/wwwroot/js/Kosystem/utils.js
+++ Kosystem/wwwroot/js/Kosystem/utils.js
@@ -3,19 +3,21 @@ window.Kosystem = {
      * @param {HTMLSelectElement} selectElement
      * @param {string} value
      */
-    selectValue: function (selectElement, value) {
+    selectValue(selectElement, value) {
         var opts = selectElement.options;
         if (!opts) {
             return false;
         }
 
-        for (var opt, j = 0; opt = opts[j]; j++) {
-            if (opt.value === value) {
-                selectElement.selectedIndex = j;
+        for (var i = selectElement.options.length - 1; i >= 0; i--) {
+            var opt = opts[i];
+
+            if (opt && opt.value === value) {
+                selectElement.selectedIndex = i;
                 return true;
             }
         }
 
         return false;
     }
-}
+};
